6a772befdd8acbca946fe4bed7ac65e7fdaf921e
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-07-12 13:50:25 +0200

    chore: replace placeholder company name

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,12 +1,12 @@
 // Importing env files here to validate on build
 import "./src/env.mjs";
-import "@acme/auth/env.mjs";
+import "@ideasphere/auth/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  transpilePackages: ["@ideasphere/api", "@ideasphere/auth", "@ideasphere/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -11,8 +11,8 @@ import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
-import { getServerSession, type Session } from "@acme/auth";
-import { prisma } from "@acme/db";
+import { getServerSession, type Session } from "@ideasphere/auth";
+import { prisma } from "@ideasphere/db";
 
 /**
  * 1. CONTEXT

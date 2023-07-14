14f34e06f15e978a18945cd541382490e965faa3
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-07-14 18:02:53 +0200

    chore: fix typescript not recognising images

---
+++ apps/expo/images.d.ts
@@ -0,0 +1,11 @@
+declare module "*.png" {
+  import { type ImageSourcePropType } from "react-native";
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module "*.jpg" {
+  import { type ImageSourcePropType } from "react-native";
+  const value: ImageSourcePropType;
+  export default value;
+}

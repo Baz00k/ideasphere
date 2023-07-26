d1890d51ad3aa02efdbfe45dd9491641863ceb4f
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-07-26 16:30:00 +0200

    fix: use client for client libraries
    feat: add branding

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,13 +1,13 @@
+"use client"
+
 import Link from "next/link"
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
 
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
-        </h1>
+      <div className="container mt-12 flex flex-col items-center justify-center gap-y-10 px-4 py-8">
+        <h1 className="text-5xl font-bold sm:text-[5rem]">IdeaSphere</h1>
         <AuthShowcase />
       </div>
     </main>
@@ -32,7 +32,7 @@ function AuthShowcase() {
   return (
     <>
       <p className="text-center text-2xl text-zinc-200">
-        {user && <span>Logged in as {user?.user_metadata?.name}</span>}
+        {user && <span>Logged in as {user?.email}</span>}
       </p>
       <button
         className="rounded-lg bg-white/10 px-10 py-3 font-semibold text-zinc-200 no-underline transition hover:bg-white/20"

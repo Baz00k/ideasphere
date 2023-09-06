7da52c45d6878b78861557b7c85dcef53f7deee2
Jakub Buzuk <gustav.buzuk@gmail.com>
2023-09-06 16:47:01 +0200

    chore: change home layout

--- apps/expo/src/app/(auth)/home.tsx
+++ apps/expo/src/app/(auth)/home.tsx
@@ -1,30 +1,53 @@
-import { Text, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { ScrollView, Text, View } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 
 import { TopWeeklyIdeas } from "~/components/ideas/topWeeklyIdeas"
 
+interface TopAction {
+  label: string
+  icon: React.ComponentProps<typeof MaterialIcons>["name"]
+  href: string
+}
+
+const TopActions: TopAction[] = [
+  {
+    label: "Wyszukaj pomysł",
+    icon: "search",
+    href: "/search",
+  },
+  {
+    label: "Top pomysły",
+    icon: "favorite",
+    href: "/top",
+  },
+  {
+    label: "Społeczność",
+    icon: "groups",
+    href: "/community",
+  },
+  {
+    label: "Dodaj pomysł",
+    icon: "add",
+    href: "/add",
+  },
+]
+
 const Home: React.FC = () => {
   return (
-    <SafeAreaView className="h-full w-full bg-white p-2">
-      <View className=" flex w-full flex-row flex-wrap">
-        <View className="aspect-square w-1/2 p-2">
-          <View className="h-full w-full rounded-3xl bg-primary"></View>
-        </View>
-        <View className="aspect-square w-1/2 p-2">
-          <View className="h-full w-full rounded-3xl bg-primary"></View>
-        </View>
-        <View className="aspect-square w-1/2 p-2">
-          <View className="h-full w-full rounded-3xl bg-primary"></View>
-        </View>
-        <View className="aspect-square w-1/2 p-2">
-          <View className="h-full w-full rounded-3xl bg-primary"></View>
-        </View>
+    <ScrollView className="h-full w-full bg-white px-2">
+      <View className="my-4 flex w-full flex-row justify-between">
+        {TopActions.map((action) => (
+          <View key={action.label} className="m-2 flex w-1/5 flex-col justify-center gap-y-2">
+            <View className="flex aspect-square w-full items-center justify-center rounded-full border-2 border-gray-500 bg-gray-50">
+              <MaterialIcons name={action.icon} size={32} color="black" />
+            </View>
+            <Text className="text-center text-xs font-medium text-secondary">{action.label}</Text>
+          </View>
+        ))}
       </View>
-      <Text className="my-8 text-center text-3xl font-light text-secondary">
-        Najpopularniejsze w tym tygodniu
-      </Text>
+
       <TopWeeklyIdeas />
-    </SafeAreaView>
+    </ScrollView>
   )
 }
 

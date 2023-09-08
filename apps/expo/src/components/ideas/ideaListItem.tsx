0eb09f0ed38e23204128dd05b6fe47e027675e0e
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-09-08 19:06:35 +0200

    feat: add ability to favourite an idea

--- apps/expo/src/components/ideas/ideaListItem.tsx
+++ apps/expo/src/components/ideas/ideaListItem.tsx
@@ -1,13 +1,17 @@
 import { Text, TouchableOpacity, View } from "react-native"
 import { Link } from "expo-router"
 import { MaterialIcons } from "@expo/vector-icons"
-import type { Prisma } from "@prisma/client"
+import type { inferRouterOutputs } from "@trpc/server"
+
+import type { AppRouter } from "@ideasphere/api"
 
 interface IdeaListItemProps {
-  item: Prisma.IdeaSelect
+  item: inferRouterOutputs<AppRouter>["ideas"]["weeklyTopIdeas"][number]
+  favourite: () => void
+  unfavourite: () => void
 }
 
-export const IdeaListItem: React.FC<IdeaListItemProps> = ({ item }) => {
+export const IdeaListItem: React.FC<IdeaListItemProps> = ({ item, favourite, unfavourite }) => {
   return (
     <Link
       href={{
@@ -24,7 +28,14 @@ export const IdeaListItem: React.FC<IdeaListItemProps> = ({ item }) => {
           <Text className="text-xl font-bold">{item.title}</Text>
           <Text className="line-clamp-1 max-h-4 text-ellipsis">{item.description}</Text>
         </View>
-        <MaterialIcons name="favorite-border" size={24} color="black" />
+        <View className="flex-column flex flex-shrink">
+          {item.favoritedBy.length > 0 ? (
+            <MaterialIcons name="favorite" size={24} color="red" onPress={unfavourite} />
+          ) : (
+            <MaterialIcons name="favorite-border" size={24} color="black" onPress={favourite} />
+          )}
+          <Text className="text-center">{item._count.favoritedBy}</Text>
+        </View>
       </TouchableOpacity>
     </Link>
   )
--- apps/expo/src/components/ideas/topWeeklyIdeas.tsx
+++ apps/expo/src/components/ideas/topWeeklyIdeas.tsx
@@ -1,19 +1,120 @@
-import { forwardRef, useImperativeHandle } from "react"
-import { Text, View } from "react-native"
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react"
+import { LayoutAnimation, Text, View } from "react-native"
 import { FlashList } from "@shopify/flash-list"
+import type { inferRouterOutputs } from "@trpc/server"
+
+import type { AppRouter } from "@ideasphere/api"
 
 import { LoadingSpinner } from "~/components/base/loadingSpinner"
 import { IdeaListItem } from "~/components/ideas/ideaListItem"
 import { api } from "~/utils/api"
 
 export interface TopWeeklyIdeasRef {
-  refetch: () => Promise<void>
+  refresh: () => Promise<void>
 }
 
-export const TopWeeklyIdeas: React.FC = forwardRef(function TopWeeklyIdeasInner(_, ref) {
-  const { data, isLoading, isError, refetch } = api.ideas.weeklyTopIdeas.useQuery()
+type FlashListData = inferRouterOutputs<AppRouter>["ideas"]["weeklyTopIdeas"][number]
 
-  useImperativeHandle(ref, () => ({ refetch }))
+export const TopWeeklyIdeas = forwardRef<TopWeeklyIdeasRef>(function TopWeeklyIdeasInner(_, ref) {
+  useImperativeHandle(ref, () => ({ refresh }))
+
+  const listRef = useRef<FlashList<FlashListData>>(null)
+  const [listData, setListData] = useState<typeof data>()
+
+  const utils = api.useContext()
+  const invalidateTopWeeklyIdeas = async () => {
+    await utils.ideas.weeklyTopIdeas.invalidate()
+  }
+
+  const { data, isLoading, isError } = api.ideas.weeklyTopIdeas.useQuery()
+  const { mutate: favourite } = api.ideas.favourite.useMutation({
+    onSuccess: invalidateTopWeeklyIdeas,
+
+    // Optimistic update - add a fake favorite to the list
+    onMutate: async (idea) => {
+      await utils.ideas.weeklyTopIdeas.cancel()
+
+      const previousData = utils.ideas.weeklyTopIdeas.getData()
+      utils.ideas.weeklyTopIdeas.setData(undefined, (data) => {
+        return data?.map((item) => {
+          if (item.id === idea.id) {
+            return {
+              ...item,
+              favoritedByMe: true,
+              _count: {
+                favoritedBy: item._count.favoritedBy + 1,
+              },
+            }
+          }
+          return item
+        })
+      })
+
+      return { previousData }
+    },
+
+    onError: (_, __, context) => {
+      utils.ideas.weeklyTopIdeas.setData(undefined, context?.previousData)
+    },
+
+    onSettled: () => {
+      void utils.ideas.weeklyTopIdeas.invalidate()
+    },
+  })
+
+  const { mutate: unfavourite } = api.ideas.unfavourite.useMutation({
+    onSuccess: invalidateTopWeeklyIdeas,
+
+    // Optimistic update - remove a fake favorite from the list
+    onMutate: async (idea) => {
+      await utils.ideas.weeklyTopIdeas.cancel()
+
+      const previousData = utils.ideas.weeklyTopIdeas.getData()
+      utils.ideas.weeklyTopIdeas.setData(undefined, (data) => {
+        return data?.map((item) => {
+          if (item.id === idea.id) {
+            return {
+              ...item,
+              favoritedByMe: false,
+              _count: {
+                favoritedBy: item._count.favoritedBy - 1,
+              },
+            }
+          }
+          return item
+        })
+      })
+
+      return { previousData }
+    },
+
+    onError: (_, __, context) => {
+      utils.ideas.weeklyTopIdeas.setData(undefined, context?.previousData)
+    },
+
+    onSettled: () => {
+      void utils.ideas.weeklyTopIdeas.invalidate()
+    },
+  })
+
+  const refresh = async () => {
+    await utils.ideas.weeklyTopIdeas.invalidate()
+  }
+
+  useEffect(() => {
+    setListData(data)
+    listRef.current?.prepareForLayoutAnimationRender()
+    LayoutAnimation.configureNext({
+      duration: 200,
+      create: {
+        type: LayoutAnimation.Types.easeInEaseOut,
+        property: LayoutAnimation.Properties.opacity,
+      },
+      update: {
+        type: LayoutAnimation.Types.easeInEaseOut,
+      },
+    })
+  }, [data])
 
   return (
     <View className="min-h-32 w-full">
@@ -22,12 +123,20 @@ export const TopWeeklyIdeas: React.FC = forwardRef(function TopWeeklyIdeasInner(
       </Text>
       {isLoading && <LoadingSpinner />}
       {isError && <ErrorView />}
-      {data && data.length === 0 && <EmptyView />}
-      {data && data.length > 0 && (
+      {listData && (
         <FlashList
-          data={data}
-          renderItem={({ item }) => <IdeaListItem item={item} />}
+          ref={listRef}
+          data={listData}
+          renderItem={({ item }) => (
+            <IdeaListItem
+              item={item}
+              favourite={() => favourite({ id: item.id })}
+              unfavourite={() => unfavourite({ id: item.id })}
+            />
+          )}
+          keyExtractor={(item) => item.id}
           estimatedItemSize={100}
+          ListEmptyComponent={EmptyView}
         />
       )}
     </View>

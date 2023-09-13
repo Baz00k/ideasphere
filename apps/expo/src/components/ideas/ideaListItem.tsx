ca5555199940c8df9b37fddff5d4ae815eb3090e
Jakub Buzuk <gustav.buzuk@gmail.com>
2023-09-13 16:52:58 +0200

    feat: add like animaiton

--- apps/expo/src/components/ideas/ideaListItem.tsx
+++ apps/expo/src/components/ideas/ideaListItem.tsx
@@ -3,6 +3,7 @@ import Animated, { BounceIn, FadeOut } from "react-native-reanimated"
 import { Link } from "expo-router"
 import { MaterialIcons } from "@expo/vector-icons"
 import type { inferRouterOutputs } from "@trpc/server"
+import tailwindColors from "tailwindcss/colors"
 
 import type { AppRouter } from "@ideasphere/api"
 import { colors } from "@ideasphere/tailwind-config/themeColors"
@@ -37,8 +38,8 @@ export const IdeaListItem: React.FC<IdeaListItemProps> = ({ item, favourite, unf
           onPress={() => (item.favoritedByMe ? unfavourite() : favourite())}
         >
           {item.favoritedByMe ? (
-            <Animated.View entering={BounceIn} exiting={FadeOut}>
-              <MaterialIcons name="favorite" size={24} color="red" />
+            <Animated.View entering={BounceIn.duration(200)} exiting={FadeOut.duration(200)}>
+              <MaterialIcons name="favorite" size={24} color={tailwindColors.red[600]} />
             </Animated.View>
           ) : (
             <MaterialIcons name="favorite-border" size={24} color={colors.primary} />
